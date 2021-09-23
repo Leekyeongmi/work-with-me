dc5e0ea996d9394524702acd7b176cd69e69f2b7
Leekyeongmi <starry3ones@gmail.com>
2021-09-23 13:03:05 +0900

    final

---
+++ js/showlink.js
@@ -0,0 +1,3 @@
+function showLink() {
+    window.open("https://us02web.zoom.us/j/83598095077?pwd=QU83TU9qVXJoelVaU0EvV1JYYVRVdz09");
+  }
---
+++ js/switch.js
@@ -0,0 +1,11 @@
+function switchHandler() {
+    if(document.getElementById('switch').className === "fas fa-toggle-on fa-lg")
+    {
+        document.getElementById('switch').className = "fas fa-toggle-off fa-lg";
+        document.querySelector('body').style.backgroundColor = "black";
+   }else{
+    document.getElementById('switch').className = "fas fa-toggle-on fa-lg";
+    document.querySelector('body').style.backgroundColor = "blue";
+   }
+}
+
