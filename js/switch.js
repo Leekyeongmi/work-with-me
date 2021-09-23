fbf101a28fc7347436f2c766ed9dac186f736f5a
Leekyeongmi <starry3ones@gmail.com>
2021-09-23 20:53:06 +0900

    hi

--- js/switch.js
+++ js/switch.js
@@ -2,10 +2,11 @@ function switchHandler() {
     if(document.getElementById('switch').className === "fas fa-toggle-on fa-lg")
     {
         document.getElementById('switch').className = "fas fa-toggle-off fa-lg";
-        document.querySelector('body').style.backgroundColor = "black";
+        document.querySelector('body').style.background = 'linear-gradient(to top, #f7ff00, #db36a4)';
+       
    }else{
     document.getElementById('switch').className = "fas fa-toggle-on fa-lg";
-    document.querySelector('body').style.backgroundColor = "blue";
+    document.querySelector('body').style.background = "blue";
    }
 }
 
