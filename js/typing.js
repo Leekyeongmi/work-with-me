05bc5cd51dce142b08a20c6c235c7c8e80b28ab1
Leekyeongmi <starry3ones@gmail.com>
2021-09-25 14:57:07 +0900

    typing up

---
+++ js/typing.js
@@ -0,0 +1,67 @@
+// get the element
+const text = document.querySelector(".typing-text");
+
+// make a words array
+const words = [
+  "✅ 9.25. SAT.",
+  "OPEN/ 8:00AM",
+  "CLOSE/ 8:00PM",
+  "repeat.",
+];
+
+// start typing effect
+setTyper(text, words);
+
+function setTyper(element, words) {
+  const LETTER_TYPE_DELAY = 75;
+  const WORD_STAY_DELAY = 1500;
+
+  const DIRECTION_FORWARDS = 0;
+  const DIRECTION_BACKWARDS = 1;
+
+  var direction = DIRECTION_FORWARDS;
+  var wordIndex = 0;
+  var letterIndex = 0;
+
+  var wordTypeInterval;
+
+  startTyping();
+
+  function startTyping() {
+    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+  }
+
+  function typeLetter() {
+    const word = words[wordIndex];
+
+    if (direction == DIRECTION_FORWARDS) {
+      letterIndex++;
+
+      if (letterIndex == word.length) {
+        direction = DIRECTION_BACKWARDS;
+        clearInterval(wordTypeInterval);
+        setTimeout(startTyping, WORD_STAY_DELAY);
+      }
+    } else if (direction == DIRECTION_BACKWARDS) {
+      letterIndex--;
+
+      if (letterIndex == 0) {
+        nextWord();
+      }
+    }
+
+    const textToType = word.substring(0, letterIndex);
+
+    element.textContent = textToType;
+  }
+
+  function nextWord() {
+    letterIndex = 0;
+    direction = DIRECTION_FORWARDS;
+    wordIndex++;
+
+    if (wordIndex == words.length) {
+      wordIndex = 0;
+    }
+  }
+}
