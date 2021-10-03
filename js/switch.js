fc8962d44ee4be2bb6f43847583d2b176a125e27
Leekyeongmi <starry3ones@gmail.com>
2021-10-03 17:42:55 +0900

    last color

--- js/switch.js
+++ js/switch.js
@@ -2,7 +2,7 @@ function switchHandler() {
     if(document.getElementById('switch').className === "fas fa-toggle-on fa-lg")
     {
         document.getElementById('switch').className = "fas fa-toggle-off fa-lg";
-        document.querySelector('body').style.background = 'linear-gradient(to top, #f7ff00, #db36a4)';
+        document.querySelector('body').style.background = 'black';
         
         // 'linear-gradient(to bottom, #8360c3, #2ebf91)';
        
